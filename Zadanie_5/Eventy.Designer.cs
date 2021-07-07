ac4313ad6f7b2fd7a47182521c8cc3623254fbd6
Daniel Huber <dannyyhub@gmail.com>
2021-07-07 22:08:39 +0200

    Add project files.

---
+++ Zadanie_5/Eventy.Designer.cs
@@ -0,0 +1,219 @@
+﻿
+namespace Zadanie_5
+{
+    partial class Zadanie_5
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxLogin = new System.Windows.Forms.TextBox();
+            this.textBoxHasloRejestracja1 = new System.Windows.Forms.TextBox();
+            this.buttonZarejestruj = new System.Windows.Forms.Button();
+            this.textBoxHasloRejestracja2 = new System.Windows.Forms.TextBox();
+            this.checkBoxRODO = new System.Windows.Forms.CheckBox();
+            this.buttonZaloguj = new System.Windows.Forms.Button();
+            this.textLogowanie = new System.Windows.Forms.TextBox();
+            this.textBoxHasloLogowanie = new System.Windows.Forms.TextBox();
+            this.labelLogin = new System.Windows.Forms.Label();
+            this.labelHaslo1 = new System.Windows.Forms.Label();
+            this.labelRODO = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxLogin
+            // 
+            this.textBoxLogin.Location = new System.Drawing.Point(44, 81);
+            this.textBoxLogin.Name = "textBoxLogin";
+            this.textBoxLogin.PlaceholderText = "Login";
+            this.textBoxLogin.Size = new System.Drawing.Size(100, 23);
+            this.textBoxLogin.TabIndex = 0;
+            // 
+            // textBoxHasloRejestracja1
+            // 
+            this.textBoxHasloRejestracja1.Location = new System.Drawing.Point(44, 135);
+            this.textBoxHasloRejestracja1.Name = "textBoxHasloRejestracja1";
+            this.textBoxHasloRejestracja1.PasswordChar = '*';
+            this.textBoxHasloRejestracja1.PlaceholderText = "Haslo";
+            this.textBoxHasloRejestracja1.Size = new System.Drawing.Size(100, 23);
+            this.textBoxHasloRejestracja1.TabIndex = 1;
+            // 
+            // buttonZarejestruj
+            // 
+            this.buttonZarejestruj.Location = new System.Drawing.Point(44, 261);
+            this.buttonZarejestruj.Name = "buttonZarejestruj";
+            this.buttonZarejestruj.Size = new System.Drawing.Size(75, 23);
+            this.buttonZarejestruj.TabIndex = 2;
+            this.buttonZarejestruj.Text = "Zarejestruj";
+            this.buttonZarejestruj.UseVisualStyleBackColor = true;
+            this.buttonZarejestruj.Click += new System.EventHandler(this.buttonZarejestruj_Click);
+            // 
+            // textBoxHasloRejestracja2
+            // 
+            this.textBoxHasloRejestracja2.Location = new System.Drawing.Point(44, 167);
+            this.textBoxHasloRejestracja2.Name = "textBoxHasloRejestracja2";
+            this.textBoxHasloRejestracja2.PasswordChar = '*';
+            this.textBoxHasloRejestracja2.PlaceholderText = "Potwierdz Haslo";
+            this.textBoxHasloRejestracja2.Size = new System.Drawing.Size(100, 23);
+            this.textBoxHasloRejestracja2.TabIndex = 3;
+            // 
+            // checkBoxRODO
+            // 
+            this.checkBoxRODO.AutoSize = true;
+            this.checkBoxRODO.Location = new System.Drawing.Point(44, 213);
+            this.checkBoxRODO.Name = "checkBoxRODO";
+            this.checkBoxRODO.Size = new System.Drawing.Size(96, 19);
+            this.checkBoxRODO.TabIndex = 4;
+            this.checkBoxRODO.Text = "Zgoda RODO";
+            this.checkBoxRODO.UseVisualStyleBackColor = true;
+            // 
+            // buttonZaloguj
+            // 
+            this.buttonZaloguj.Location = new System.Drawing.Point(606, 187);
+            this.buttonZaloguj.Name = "buttonZaloguj";
+            this.buttonZaloguj.Size = new System.Drawing.Size(75, 23);
+            this.buttonZaloguj.TabIndex = 5;
+            this.buttonZaloguj.Text = "Zaloguj";
+            this.buttonZaloguj.UseVisualStyleBackColor = true;
+            this.buttonZaloguj.Click += new System.EventHandler(this.buttonZaloguj_Click);
+            // 
+            // textLogowanie
+            // 
+            this.textLogowanie.Location = new System.Drawing.Point(606, 81);
+            this.textLogowanie.Name = "textLogowanie";
+            this.textLogowanie.PlaceholderText = "Login";
+            this.textLogowanie.Size = new System.Drawing.Size(100, 23);
+            this.textLogowanie.TabIndex = 6;
+            // 
+            // textBoxHasloLogowanie
+            // 
+            this.textBoxHasloLogowanie.Location = new System.Drawing.Point(606, 135);
+            this.textBoxHasloLogowanie.Name = "textBoxHasloLogowanie";
+            this.textBoxHasloLogowanie.PasswordChar = '*';
+            this.textBoxHasloLogowanie.PlaceholderText = "Haslo";
+            this.textBoxHasloLogowanie.Size = new System.Drawing.Size(100, 23);
+            this.textBoxHasloLogowanie.TabIndex = 7;
+            // 
+            // labelLogin
+            // 
+            this.labelLogin.AutoSize = true;
+            this.labelLogin.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelLogin.ForeColor = System.Drawing.Color.Maroon;
+            this.labelLogin.Location = new System.Drawing.Point(170, 84);
+            this.labelLogin.Name = "labelLogin";
+            this.labelLogin.Size = new System.Drawing.Size(93, 15);
+            this.labelLogin.TabIndex = 8;
+            this.labelLogin.Text = "Login jest pusty";
+            this.labelLogin.Visible = false;
+            // 
+            // labelHaslo1
+            // 
+            this.labelHaslo1.AutoSize = true;
+            this.labelHaslo1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelHaslo1.ForeColor = System.Drawing.Color.Maroon;
+            this.labelHaslo1.Location = new System.Drawing.Point(170, 154);
+            this.labelHaslo1.Name = "labelHaslo1";
+            this.labelHaslo1.Size = new System.Drawing.Size(124, 15);
+            this.labelHaslo1.TabIndex = 9;
+            this.labelHaslo1.Text = "Hasla sie nie zgadzaja";
+            this.labelHaslo1.Visible = false;
+            // 
+            // labelRODO
+            // 
+            this.labelRODO.AutoSize = true;
+            this.labelRODO.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelRODO.ForeColor = System.Drawing.Color.Maroon;
+            this.labelRODO.Location = new System.Drawing.Point(170, 213);
+            this.labelRODO.Name = "labelRODO";
+            this.labelRODO.Size = new System.Drawing.Size(172, 15);
+            this.labelRODO.TabIndex = 10;
+            this.labelRODO.Text = "CheckBox nie jest zaznaczony";
+            this.labelRODO.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(29, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 32);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Rejestracja";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(589, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 32);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Logowanie";
+            // 
+            // Zadanie_5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelRODO);
+            this.Controls.Add(this.labelHaslo1);
+            this.Controls.Add(this.labelLogin);
+            this.Controls.Add(this.textBoxHasloLogowanie);
+            this.Controls.Add(this.textLogowanie);
+            this.Controls.Add(this.buttonZaloguj);
+            this.Controls.Add(this.checkBoxRODO);
+            this.Controls.Add(this.textBoxHasloRejestracja2);
+            this.Controls.Add(this.buttonZarejestruj);
+            this.Controls.Add(this.textBoxHasloRejestracja1);
+            this.Controls.Add(this.textBoxLogin);
+            this.Name = "Zadanie_5";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxLogin;
+        private System.Windows.Forms.TextBox textBoxHasloRejestracja1;
+        private System.Windows.Forms.Button buttonZarejestruj;
+        private System.Windows.Forms.TextBox textBoxHasloRejestracja2;
+        private System.Windows.Forms.CheckBox checkBoxRODO;
+        private System.Windows.Forms.Button buttonZaloguj;
+        private System.Windows.Forms.TextBox textLogowanie;
+        private System.Windows.Forms.TextBox textBoxHasloLogowanie;
+        private System.Windows.Forms.Label labelLogin;
+        private System.Windows.Forms.Label labelHaslo1;
+        private System.Windows.Forms.Label labelRODO;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Zadanie_5/Eventy.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadanie_5
+{
+    public partial class Zadanie_5 : Form
+    {
+        public Zadanie_5()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public struct User
+        {
+            public string login;
+            public string haslo;
+
+        }
+
+        List<User> users_list = new List<User>();
+
+
+
+        private void buttonZarejestruj_Click(object sender, EventArgs e)
+        {
+
+
+            /*    ------Żeby utworzyć konto, Login nie może być pusty, hasła muszą się zgadzać a Checkbox musi być zaznaczony. -----   */
+
+
+            // Tablica booleanow potrzebna do walidacji
+
+            bool[] goodToGo = new bool[3];
+
+            // login 
+
+            if (textBoxLogin.TextLength == 0)
+            {
+                labelLogin.Visible = true;
+
+            }
+            else
+            {
+                goodToGo[0] = true;
+                labelLogin.Visible = false;
+            }
+
+            // hasla
+
+            if ((textBoxHasloRejestracja1.TextLength == 0) || (textBoxHasloRejestracja2.TextLength == 0) || (textBoxHasloRejestracja1.Text != textBoxHasloRejestracja2.Text))
+            {
+                labelHaslo1.Visible = true;
+
+            }
+            else
+            {
+                goodToGo[1] = true;
+                labelHaslo1.Visible = false;
+            }
+
+            // checkbox
+
+            if (checkBoxRODO.Checked == false)
+            {
+                labelRODO.Visible = true;
+
+            }
+            else
+            {
+                goodToGo[2] = true;
+                labelRODO.Visible = false;
+            }
+
+            // kiedy cała tablica jest ustawiona na 1 wtedy dodaje nowego usera
+
+            if (goodToGo.Contains(false) == false)
+            {
+                label1.Text = "Dziala";
+
+                // Po zarejestrowaniu wyświetl MessageBox z informacją o poprawnym zarejestrowaniu.
+
+                MessageBox.Show("Dodano Klienta", "Sukces");
+
+
+                // tutaj jakies dodawanie do kolekcji, potem przy logowaniu uzyje się danych z rejestracji
+                users_list.Add(new User() { login = textBoxLogin.Text, haslo = textBoxHasloRejestracja1.Text });
+
+            }
+
+        }
+
+
+        /*
+            W sekcji logowania dodaj tylko Textboxy "Login", "Hasło" i Button "Zaloguj". 
+            Pole z hasłem musi ukrywać znaki. Dane muszą należeć do istniejącego konta. 
+            Wyświetl MessageBox z powiadomieniem
+        */
+
+
+        private void buttonZaloguj_Click(object sender, EventArgs e)
+        {
+            bool znaleziono = false; // tego boola potrzebuje do sprawdzenia czy znaleziono usera w foreach-u
+
+            // sprawdzam czy lista jest pusta
+
+            if (users_list.Count == 0 )
+            {
+                MessageBox.Show("Lista userow jest pusta", "Blad");
+            }
+
+            // jesli nie to sprawdzam czy jest taki user
+
+            else
+            {
+                foreach (var x in users_list)
+                {
+                    if ((textBoxHasloLogowanie.Text == x.haslo) && (textLogowanie.Text == x.login))
+                    {
+                        MessageBox.Show("Zalogowano sie na usera ktory zostal zarejestrowany ", "Sukces");
+                        znaleziono = true;
+                        break;
+                    }
+
+                }
+                if (znaleziono == false) MessageBox.Show("Brak takeigo usera albo niepoprawne dane", "Blad");
+            }
+
+
+           
+
+        }
+
+
+
+    }
+}
